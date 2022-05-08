acfc985bbfa2c47754b25115d240784ca9eceb85
rkshristi123 <shristikumari759@gmail.com>
2022-05-08 17:08:53 +0530

    .

--- scripts/movies.js
+++ scripts/movies.js
@@ -9,7 +9,7 @@ document.querySelector('#footer_top').innerHTML=footer();
 //.........................................................//
 //slider
 import { movieslider,swiperfn } from '../components/movie_slider.js';
-document.getElementById('swiper').innerHTML=movieslider();
+document.getElementById('slider').innerHTML=movieslider();
 swiperfn();
 //featured celibrities slider
 import { celibrity_slider,celibfn } from '../components/celibrity_slider.js';
